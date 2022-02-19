bcaef685371475a2c340b38c17a45590ad9f687c
Alvaro Esteban Garzon Ramirez <nanolanda05@gmail.com>
2022-02-19 15:57:26 +0100

    Initial commit and project components created

---
+++ src/data/monedas.js
@@ -0,0 +1,6 @@
+export const monedas = [
+    { id: 'USD', nombre: 'Dolar de Estados Unidos'},
+    { id: 'COP', nombre: 'Pesos Colombianos'},
+    { id: 'EUR', nombre: 'Euros'},
+    { id: 'GBP', nombre: 'Libra Esterlina'}
+]
